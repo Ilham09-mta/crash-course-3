dd9336c223d4627db190fe12bd5d27b3f358643b
Ilham Saifullah <muhammad.ilham.saifullah@mail.ugm.ac.id>
2021-02-17 09:43:47 +0700

    - perubahan kode

--- use_requests.py
+++ use_requests.py
@@ -3,7 +3,7 @@ import requests
 url = 'https://detik.com'
 try:
     response = requests.get(url)
-    if response.status_code ==200:
+    if response.status_code == 200:
         print('Success! Response= {response.status_code}')
         print(f'Content {response.text}')
     else:
